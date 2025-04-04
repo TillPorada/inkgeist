ba1acf028da2cbe20693f907efe4e3ec3784c931
Till Porada <till@adarop.de>
2025-04-04 21:31:21 +0200

    fix

--- client/src/main.ts
+++ client/src/main.ts
@@ -1,7 +1,8 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
-import vuetify from '@/plugins/vuetify'
+import 'vuetify/styles'
+import vuetify from 'plugins/vuetify'
 
 createApp(App)
   .use(router)
