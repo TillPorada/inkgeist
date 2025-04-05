8a48ae1de9f28150b597d18b6b84f93e1c4d0d61
Till Porada <till@adarop.de>
2025-04-05 14:53:45 +0200

    add routerview

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,10 +1,9 @@
 <template>
   <div id="app">
     <h1>Inkgeist Frontend</h1>
-    <UserList />
+    <router-view></router-view>
   </div>
 </template>
 
 <script setup lang="ts">
-import UserList from '@/components/UserLIst.vue'
 </script>
